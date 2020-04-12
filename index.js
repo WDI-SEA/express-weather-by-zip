f2801a4bc60e9ed6435f58932e27ffb78f4094e0
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-04-12 14:42:56 -0700

    functional weather app

---
+++ index.js
@@ -0,0 +1,38 @@
+let express = require('express')
+let app = express()
+let weather = require('weather-js')
+
+app.set('view engine', 'ejs')
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    res.render('home',{message: ''})
+})
+
+app.get('/weather', (req, res) => {
+    // Run the zip code through weather-js, and expect a return
+    let zipcode = req.query.zip
+    weather.find({search: zipcode, degreeType: 'F'}, (err, result) => {
+       
+        if(err) {
+            console.log('error',err)        
+        }
+        else if(Object.keys(result).length != 0){
+            console.log("rest not empty",result)
+            res.render('results', {  location: result[0].location.name,
+                                    zipcode: result[0].location.zipcode,
+                                    current: result[0].current,
+                                    forecast: result[0].forecast
+                                }) 
+        }
+        else{
+             res.render('home',{message: 'Not a valid zipcode!'})
+        }
+    })
+})
+
+
+
+app.listen(8000, () => {
+    console.log('Listening and cruising on port 8000')
+})
