6485359511cf689b1c06658f587bbfb2c350fcc4
Jummay77 <hansangho@yahoo.com>
2020-04-20 16:17:29 -0700

    functional with no styling

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 let express = require('express')
-let app = express()
+let app = express() 
 let weather = require('weather-js')
 
 app.set('view engine', 'ejs')
 app.use(express.static('static'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home') 
 })
 
 app.get('/weather', (req, res) => {
-    // Run the zip code through weather-js, and expect a return
+    //take the req.body.zip and pack it into a variable 
+    //make sure the data is coming from the front end correctly "parse.json"
+    //Run the zip thru the weather-js, and expect a r/n
     let zipcode = req.query.zip
     weather.find({search: zipcode, degreeType: 'F'}, (err, result) => {
-        if(err) {
+        if(err){
             console.log(err)
         }
-        res.render('result', {  location: result[0].location.name,
-                                zipcode: result[0].location.zipcode,
-                                temp: result[0].current.temperature
+        res.render('weather', {location: result[0].location.name, 
+                              zipcode: result[0].location.zipcode, 
+                              temp: result[0].current.temperature
                             })
     })
-})
-app.listen(8000, () => {
-    console.log('Listening and cruising on port 8000')
+}) 
+app.listen(3000, () => {
+    console.log('temp?')
 })
