5984475a323e9d243ce777ffebd2547cb51f28d2
Emily Krauss <e.krauss27@gmail.com>
2020-07-26 22:49:31 -0700

    sortakindaworks but the app crashes as soon as we render weather.ejs

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 //import express
 var express = require('express')
+var bodyParser = require('body-parser')
 
 // create an instance of express
 var app = express();
+
+var jsonParser = bodyParser.json()
+ 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs')
 
 var weather = require('weather-js')
 
-app.get('/', function(req, res){
-    res.render('homepage.ejs')
+app.get('/', (req, res) => {
+  res.render('homepage')
 })
 
-app.post('/', function(req, res){
-    weather.find({search: req.query.zipcode, degreeType: 'F'}, function(err, result) {
-        if(err) console.log(err);
-       
-        console.log(JSON.stringify(result, null, 2));
-      });
-    res.render('weather')
-    console.log("hello");
+app.post('/', urlencodedParser,(req, res) => {
+  let zip = req.body.zipcode
+  res.redirect(`/weather/${zip}`)
 })
 
-
-app.get('/weather', function(req, res){
-  res.render('weather')
+app.get('/weather/:zip', (req, res) => {
+  let zipcode = req.params.zip
+  weather.find({search: zipcode, degreeType: 'F'}, (err, result) => {
+      if(err) {
+          console.log(err)
+      }
+      res.render('weather', {
+          location: result[0].location.name,
+          zip: result[0].location.zipcode,
+          temp: result[0].current.temperature,
+          feelslike: result[0].current.feelslike,
+          day: result[0].current.day,
+          skytext: result[0].current.skytext, 
+      })
+  })
 })
 
-app.get('/weather/:zipcode', function(req, res) {
-  res.render('zipdata')
-})
-
-// let app know which port to listen on
-app.listen(3000);
+app.listen(3000, console.log("listening on port 3000 🎷🐍"));
