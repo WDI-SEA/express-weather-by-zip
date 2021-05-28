82ecda6d5882c0292f1cd3719b6d5d90af17608e
erikwinkel <erikpwinkel@gmail.com>
2021-05-27 20:05:03 -0700

    added a Random Zip Code button

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ app.get('/', (req,res) => {
 
 app.get('/weather', (req,res) => {
     weather.find({search: req.query.zipcode, degreeType: 'F'}, (err,result) => {
-        if(err) console.error(err)
+        if(err) console.log(err)
         res.send(result)
     })
 })
 
 app.get('/weather/:zipcode', (req,res) => {
     weather.find({search: req.params.zipcode, degreeType: 'F'}, (err,result) => {
-        if(err) console.error(err)
-        res.send(result)
+        if(err) console.log(err)
+        if(result) res.send(result)
+        else res.send(`no weather data found for ${req.params.zipcode}`)
     })
+})
+
+app.get('/random', (req,res) => {
+    res.redirect(`/weather/${Math.ceil(Math.random()*100000)}`)
 })
 
 app.listen(PORT, () => console.log(`listening in port:${PORT}`))
