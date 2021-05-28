72aee54656ffaf64f1d37c3c1bc7eaa56edb1101
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-28 10:05:07 -0500

    Full solve, no bonus, fixed possible timezone bug.

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const weather = require("weather-js")
 const app = express()
 const PORT = 9001
 
-const now = dayjs()
-const dateFormat = 'dddd, MMMM D[,] YYYY'
-
 const log = console.log
 
+// HOME PAGE
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.get("/weather", (req, res) => {
+// SUBMIT PAGE
+app.get("/weather/", (req, res) => {
     let zipcode = req.query.zipcode
 
         weather.find({search: zipcode, degreeType: "F"}, function (err, result) {
@@ -21,8 +20,11 @@ app.get("/weather", (req, res) => {
 
             let city = result[0].location.name
             let currentTemp = result[0].current.temperature
+            let currentDate = dayjs(result[0].current.date)
             let skyText = result[0].current.skytext
             let windDisplay = result[0].current.winddisplay
+
+            const dateFormat = 'dddd, MMMM D[,] YYYY'
 
     res.writeHead(200, {"Content-Type": "text/html"})
     res.write("<!DOCTYPE html>")
@@ -41,7 +43,47 @@ app.get("/weather", (req, res) => {
     res.write(`<h3>${city}</h3>`)
     res.write(`<p>${skyText}</p>`)
     res.write(`<h1>${currentTemp} F</h1>`)
-    res.write(`<p>${now.format(dateFormat)}</p>`)
+    res.write(`<p>${currentDate.format(dateFormat)}</p>`)
+    res.write(`<p>Wind: ${windDisplay}</p>`)
+    res.write("</body>")
+    res.write("</html>")
+    res.end()
+    })
+})
+
+// URL PARAM PAGE
+app.get("/weather/:zipcode", (req, res) => {
+    let zipcode = req.params.zipcode
+
+        weather.find({search: zipcode, degreeType: "F"}, function (err, result) {
+        if(err) log(err)
+
+        let city = result[0].location.name
+        let currentTemp = result[0].current.temperature
+        let currentDate = dayjs(result[0].current.date)
+        let skyText = result[0].current.skytext
+        let windDisplay = result[0].current.winddisplay
+
+        const dateFormat = 'dddd, MMMM D[,] YYYY'
+
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.write("<!DOCTYPE html>")
+    res.write("<html>")
+    res.write("<head>")
+    res.write("<style>")
+    res.write("* {text-align: center; font-family: Arial, Helvetica, sans-serif}")
+    res.write("h3 {margin-top: 200px}")
+    res.write("h1 {font-size: 40px}")
+    res.write("</style>")
+    res.write("<title>")
+    res.write("Local Weather Data")
+    res.write("</title>")
+    res.write("</head>")
+    res.write("<body>")
+    res.write(`<h3>${city}</h3>`)
+    res.write(`<p>${skyText}</p>`)
+    res.write(`<h1>${currentTemp} F</h1>`)
+    res.write(`<p>${currentDate.format(dateFormat)}</p>`)
     res.write(`<p>Wind: ${windDisplay}</p>`)
     res.write("</body>")
     res.write("</html>")
