cbbc6039cc7c6110224391ff8e7c15ffe1bfa7fa
jaackie25 <jackiedinh91@gmail.com>
2021-05-27 21:28:38 -0700

    weather solution

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const weather = require('weather-js')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+
+app.get("/weather", (req, res) =>{
+   let zipcode = req.query.zipcode
+   weather.find({search: zipcode, degreeType: 'F'}, function(err, result) {
+    if(err) console.log(err);
+   
+    res.send(JSON.stringify(result, null, 2))
+  });
+ 
+})
+
+app.get("/weather/:zipcode", (req, res) => {
+    let zipcode = req.params.zipcode
+    weather.find({search: zipcode , degreeType: 'F'}, function(err, result) {
+     if(err) console.log(err);
+    
+     res.send(JSON.stringify(result, null, 2))
+   });
+})
+
+app.listen(9000, ()=>{
+    console.log("port is working")
+})
